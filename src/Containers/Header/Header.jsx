56c3f3456f6b24fc999026fe85591cdd4dafb9db
Akay999 <akaygamer199@gmail.com>
2022-06-04 01:51:01 +0530

    changes showSkill in header from blockchain to competetive programmer

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
 
           <div className="tag-cmp app__flex">
             <p  className='p-text'>Web Developer</p>
-            <p  className='p-text'>Blockchain Developer</p>
+            <p  className='p-text'>Competetive Programmer</p>
           </div>
         </div>
       </motion.div>
